f69d6b6f94ce35353875fe6556e3b928cd8f097f
Guypetch Mamorchok <fleabagxp@gmail.com>
2019-03-01 16:31:23 +0700

    edit port

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('bnk48_members.json')
 const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 5000
  
 server.use(middlewares)
 server.use(router)
-server.listen(5000, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
